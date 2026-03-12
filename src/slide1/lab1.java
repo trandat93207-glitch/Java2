6207fc6142f5460560c0566f0955a7e0daf8255f
TRẦN QUỐC ĐẠT <trandat93207@gmail.com>
2026-03-12 08:50:45 +0700

    Upload source code

---
+++ src/slide1/lab1.java
@@ -0,0 +1,15 @@
+package slide1;
+
+public class lab1 {
+    public static void main(String[] args) {
+
+        Xe xe = new Xe();
+
+        xe.sethangXe("Honda");
+        xe.setnamSanXuat(2025);
+
+        xe.xuat();
+
+        System.out.println("9	PS48180	Trần Quốc Đạt");
+    }
+}
